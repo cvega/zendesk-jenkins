57e45db2ab42702dff71605c4aac81f508beee74
Casey Vega <cvega@users.noreply.github.com>
2018-09-26 16:08:02 -0700

    Update samsonDeploy.groovy

--- vars/samsonDeploy.groovy
+++ vars/samsonDeploy.groovy
@@ -1,6 +1,6 @@
 def call(Map config = [:]) {
   def req = new URL("http://samson.zd-mini.com/integrations/generic/${config.ci_webhook}").openConnection();
-  def message = "{\"sha\":\""${env.GIT_COMMIT}"\",\"message\":\"hello!\"}}}"
+  def message = "{\"deploy\":{\"branch\":\""${env.GIT_LOCAL_BRANCH}"\",\"commit\": {\"sha\":\""${env.GIT_COMMIT}"\",\"message\":\"hello!\"}}}"
     req.setRequestMethod("POST")
     req.setDoOutput(true)
     req.setRequestProperty("Content-Type", "application/json")
