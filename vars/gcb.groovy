5c058280355bd7b1415ade6bb4c66109e56718f2
Casey Vega <cvega@users.noreply.github.com>
2018-09-25 17:00:46 -0700

    Create gcb.groovy

---
+++ vars/gcb.groovy
@@ -0,0 +1,11 @@
+def call(Map config = [:]) {
+  try {
+    googleCloudBuild \
+      credentialsId: ${config.credential},
+      source: local(${config.source}),
+      request: file('cloudbuild.yaml'),
+  }
+  catch(err) {
+    echo "gcb failed: ${err}"
+  }
+}
