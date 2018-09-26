bd1a5e9014d7e2de2e113390aef2e3f5518e7146
Casey Vega <cvega@users.noreply.github.com>
2018-09-26 15:24:12 -0700

    Update samsonDeploy.groovy

--- vars/samsonDeploy.groovy
+++ vars/samsonDeploy.groovy
@@ -1,12 +1,13 @@
 def call(Map config = [:]) {
-  try {   
-   def ids = sh """curl \
+  try {
+   container('curl') {
+     def ids = sh """curl \
        -d '{"deploy":{"branch":"master","commit":{"sha":"${env.GIT_COMMIT}","message":"hello!"}}}' \
        -H "Authorization: Basic 2a7da312649b74ad67197520534249637090166da1e9e858ed9179208fe030f7" \
        -H "Content-Type: application/json" \
        -X POST http://samson.zd-mini.com/integrations/generic/ | \
        jq '.deploy_ids[]' """  
-     echo ids
+   } 
   }
   catch (err) {
     throw ${err}
