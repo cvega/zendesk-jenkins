641cb3564b466f192f5bea052c550d823d4523b5
Casey Vega <cvega@users.noreply.github.com>
2018-09-26 09:58:52 -0700

    Update gcb.groovy

--- vars/gcb.groovy
+++ vars/gcb.groovy
@@ -1,4 +1,16 @@
 def call(Map config = [:]) {
-    echo "Hello Library"
-    googleCloudBuild credentialsId: "${config.credential}", source: local("${config.source}"), request: file('cloudbuild.yaml')
+  echo "Hello GCB Library"
+  try {
+    googleCloudBuild \
+      credentialsId: "${config.credentialsId}",
+      source: repo(projectId: "${config.projectId}",
+                    repoName: "${config.repoName}",
+                         tag: "${config.tag}", 
+                      commit: "${config.commitRef}"),
+      request: file("${config.cloudBuildFile}")
+    )
+  }
+  catch (err) {
+    echo "GoogleCloudBuilder failed: ${err}"
+  }
 }
