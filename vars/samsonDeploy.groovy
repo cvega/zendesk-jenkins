f0ab938d030e413916f5c61099f80d5d6479e3eb
Casey Vega <cvega@users.noreply.github.com>
2018-09-26 18:55:34 -0700

    Update samsonDeploy.groovy

--- vars/samsonDeploy.groovy
+++ vars/samsonDeploy.groovy
@@ -2,13 +2,15 @@ import groovy.json.JsonSlurperClassic
 
 
 def call(Map config = [:]) {
+  def msg = "{\"deploy\":{\"branch\": \"${env.BRANCH_NAME}\",\"commit\": {\"sha\":\"${env.GIT_COMMIT}\",\"message\":\"hello!\"}}}"
+  
+  println "${msg}"
   def req = new URL("http://samson.zd-mini.com/integrations/generic/${config.ci_webhook}").openConnection();
-  def message = "{\"deploy\":{\"branch\":\"${env.GIT_LOCAL_BRANCH}\",\"commit\": {\"sha\":\"${env.GIT_COMMIT}\",\"message\":\"hello!\"}}}"
     req.setRequestMethod("POST")
     req.setDoOutput(true)
     req.setRequestProperty("Content-Type", "application/json")
     req.setRequestProperty("Authorization", "Basic ${config.token}")
-    req.getOutputStream().write(message.getBytes("UTF-8"));
+    req.getOutputStream().write(msg.getBytes("UTF-8"));
     def postRC = req.getResponseCode();
     if(postRC.equals(200)) {
       def data = new JsonSlurperClassic().parseText(req.getInputStream().getText())
