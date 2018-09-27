dac5a7b8fa25f332ddd43547f543fd7bca0e2e6a
Casey Vega <cvega@users.noreply.github.com>
2018-09-26 17:25:07 -0700

    Update samsonDeploy.groovy

--- vars/samsonDeploy.groovy
+++ vars/samsonDeploy.groovy
@@ -13,6 +13,6 @@ def call(Map config = [:]) {
     if(postRC.equals(200)) {
       def data = new JsonSlurperClassic().parseText(post.getInputStream().getText())
       println "data.deploy_ids[0]"
-      println(post.getInputStream().getText());
+      println(req.getInputStream().getText());
     }
 }
