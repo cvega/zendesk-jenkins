dde20d81101218d4810016d50ca4fb6ae46da686
Casey Vega <cvega@users.noreply.github.com>
2018-09-26 19:49:25 -0700

    Update samsonDeploy.groovy

--- vars/samsonDeploy.groovy
+++ vars/samsonDeploy.groovy
@@ -1,19 +1,32 @@
 import groovy.json.JsonSlurperClassic
+import groovy.json.JsonBuilder
 
 
-def call(Map config = [:]) {
-  def msg = "{\"deploy\":{\"branch\": \"${env.BRANCH_NAME}\",\"commit\": {\"sha\":\"${env.GIT_COMMIT}\",\"message\":\"hello!\"}}}"
+def call(Map conf = [:]) {
+  def builder = new groovy.json.JsonBuilder()
+  def json = builder.deploy {
+    branch "${env.BRANCH_NAME}"
+    commit (
+      sha: "${env.GIT_COMMIT}",
+      message: "${conf.message}"
+    )
+  }
   
-  println "${msg}"
+  println json.toString()
+  
+  def msg = "{\"deploy\":{\"branch\": \"${env.BRANCH_NAME}\",\"commit\": {\"sha\":\"${env.GIT_COMMIT}\",\"message\":\"hello!\"}}}"  
+  
   def req = new URL("http://samson.zd-mini.com/integrations/generic/${config.ci_webhook}").openConnection();
-    req.setRequestMethod("POST")
-    req.setDoOutput(true)
-    req.setRequestProperty("Content-Type", "application/json")
-    req.setRequestProperty("Authorization", "Basic ${config.token}")
-    req.getOutputStream().write(msg.getBytes("UTF-8"));
-    def postRC = req.getResponseCode();
-    if(postRC.equals(200)) {
-      def data = new JsonSlurperClassic().parseText(req.getInputStream().getText())
-      println "${data.deploy_ids}"
-    }
+  req.setRequestMethod("POST")
+  req.setDoOutput(true)
+  req.setRequestProperty("Content-Type", "application/json")
+  req.setRequestProperty("Authorization", "Basic ${config.token}")
+  req.getOutputStream().write(msg.getBytes("UTF-8"));
+  
+  def resp = req.getResponseCode();
+  if(resp.equals(200)) {
+    def data = new JsonSlurperClassic().parseText(req.getInputStream().getText())
+  }
+  
+  
 }
