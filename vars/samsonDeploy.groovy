632decd8a42a0d7d747f1f0c2084678b18f9cc50
Casey Vega <cvega@users.noreply.github.com>
2018-09-27 08:42:05 -0700

    Update samsonDeploy.groovy

--- vars/samsonDeploy.groovy
+++ vars/samsonDeploy.groovy
@@ -7,19 +7,9 @@ def call(Map conf = [:]) {
       conf.message = "${env.BUILD_URL}"
   }
   
-  def msg = """
-  {
-    "deploy": {
-      "branch": ${env.BRANCH_NAME}
-      "commit": {
-        "sha": ${env.GIT_COMMIT},
-        "message": ${conf.message}
-      }
-    }
-  }
-  """
-    
-  def reqDeploy = new URL("http://samson.zd-mini.com/integrations/generic/${conf.ci_webhook}").openConnection();
+  def msg = "{\"deploy\":{\"branch\": \"${env.BRANCH_NAME}\",\"commit\": {\"sha\":\"${env.GIT_COMMIT}\",\"message\":\"${conf.message}\"}}}"
+  
+  def reqDeploy = new URL("http://samson.zd-mini.com/integrations/generic/${conf.webhook}").openConnection();
   reqDeploy.setRequestMethod("POST")
   reqDeploy.setDoOutput(true)
   reqDeploy.setRequestProperty("Content-Type", "application/json")
@@ -31,19 +21,16 @@ def call(Map conf = [:]) {
     def dataDeploy = new JsonSlurperClassic().parseText(reqDeploy.getInputStream().getText())
   }
     
-  def repoName = scm.getUserRemoteConfigs()[0].getUrl().tokenize('/').last().split("\\.")
-  retry(3) {
-    def reqVerify = new URL("http://samson.zd-mini.com/projects/${repoName}/deploys/180.json").openConnection();
-    reqVerify.setRequestMethod("GET")
-    reqVerify.setDoOutput(true)
-    reqVerify.setRequestProperty("Content-Type", "application/json")
-    reqVerify.setRequestProperty("Authorization", "Bearer ${conf.token}")
-    reqVerify.getOutputStream()
+  def repoName = "hello-cje" //scm.getUserRemoteConfigs()[0].getUrl().tokenize('/').last().split("\\.")
+  
+  def reqVerify = new URL("http://samson.zd-mini.com/projects/${repoName}/deploys/${dataDeploy.deploy_ids[0]}.json").openConnection();
+  reqVerify.setRequestMethod("GET")
+  reqVerify.setRequestProperty("Content-Type", "application/json")
+  reqVerify.setRequestProperty("Authorization", "Bearer ${conf.token}")
     
-    def respVerify = reqVerify.getResponseCode();
-    if(respVerify.equals(200)) {
-      def dataVerify = new JsonSlurperClassic().parseText(reqVerify.getInputStream().getText())
-      println "${dataVerify}"
-    }
+  def respVerify = reqVerify.getResponseCode();
+  if(respVerify.equals(200)) {
+    def dataVerify = new JsonSlurperClassic().parseText(reqVerify.getInputStream().getText())
+    println "${dataVerify}"
   }
 }
