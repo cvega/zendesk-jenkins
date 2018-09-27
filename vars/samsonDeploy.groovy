b7987475a67d39e9d80af03ea8af3912b90e546e
Casey Vega <cvega@users.noreply.github.com>
2018-09-26 23:23:07 -0700

    Update samsonDeploy.groovy

--- vars/samsonDeploy.groovy
+++ vars/samsonDeploy.groovy
@@ -8,27 +8,41 @@ def call(Map conf = [:]) {
   }
   
   def msg = """
-    {
-        "deploy": {
-            "branch": ${env.BRANCH_NAME}
-            "commit": {
-                "sha": ${env.GIT_COMMIT},
-                "message": ${conf.message}
-            }
-        }
+  {
+    "deploy": {
+      "branch": ${env.BRANCH_NAME}
+      "commit": {
+        "sha": ${env.GIT_COMMIT},
+        "message": ${conf.message}
+      }
     }
+  }
   """
     
-  def req = new URL("http://samson.zd-mini.com/integrations/generic/${conf.ci_webhook}").openConnection();
-  req.setRequestMethod("POST")
-  req.setDoOutput(true)
-  req.setRequestProperty("Content-Type", "application/json")
-  req.setRequestProperty("Authorization", "Basic ${conf.token}")
-  req.getOutputStream().write(msg.getBytes("UTF-8"));
+  def reqDeploy = new URL("http://samson.zd-mini.com/integrations/generic/${conf.ci_webhook}").openConnection();
+  reqDeploy.setRequestMethod("POST")
+  reqDeploy.setDoOutput(true)
+  reqDeploy.setRequestProperty("Content-Type", "application/json")
+  reqDeploy.setRequestProperty("Authorization", "Basic ${conf.token}")
+  reqDeploy.getOutputStream().write(msg.getBytes("UTF-8"));
   
-  def resp = req.getResponseCode();
-  if(resp.equals(200)) {
-    def data = new JsonSlurperClassic().parseText(req.getInputStream().getText())
-  }  
+  def respDeploy = reqDeploy.getResponseCode();
+  if(respDeploy.equals(200)) {
+    def dataDeploy = new JsonSlurperClassic().parseText(reqDeploy.getInputStream().getText())
+  }
+    
+  def repoName = build.environment.get("GIT_URL").replaceAll('https://github.com/zendesk/', '').replaceAll('.git', '')"
+  retry(3) {
+    def reqVerify = new URL("http://samson.zd-mini.com/projects/${repoName}/deploys/180.json").openConnection();
+    reqVerify.setRequestMethod("GET")
+    reqVerify.setRequestProperty("Content-Type", "application/json")
+    reqVerify.setRequestProperty("Authorization", "Bearer ${conf.token}")
+    reqVerify.getOutputStream()
+    
+    def respVerify = reqVerify.getResponseCode();
+    if(respVerify.equals(200)) {
+    def dataVerify = new JsonSlurperClassic().parseText(reqVerify.getInputStream().getText())
+        println "${dataVerify}"
+    }
 }
 
