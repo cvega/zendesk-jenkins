55c9b978f9c3f3cf5e8c5442484bcd59d3a40c2a
Casey Vega <cvega@users.noreply.github.com>
2018-09-28 01:54:35 -0700

    Update samsonDeploy.groovy

--- vars/samsonDeploy.groovy
+++ vars/samsonDeploy.groovy
@@ -1,36 +1,9 @@
-import groovy.json.JsonSlurperClassic
+import zendesk.samson.Deploy
 
 
 def call(Map conf = [:]) {
-    
-  if (!conf.message) {
-      conf.message = "${env.BUILD_URL}"
-  }
-  
-  def msg = "{\"deploy\":{\"branch\": \"${env.BRANCH_NAME}\",\"commit\": {\"sha\":\"${env.GIT_COMMIT}\",\"message\":\"${conf.message}\"}}}"
-  
-  def reqDeploy = new URL("http://samson.zd-mini.com/integrations/generic/${conf.webhook}").openConnection();
-  reqDeploy.setRequestMethod("POST")
-  reqDeploy.setDoOutput(true)
-  reqDeploy.setRequestProperty("Content-Type", "application/json")
-  reqDeploy.setRequestProperty("Authorization", "Basic ${conf.token}")
-  reqDeploy.getOutputStream().write(msg.getBytes("UTF-8"));
-  
-  def respDeploy = reqDeploy.getResponseCode();
-  if(respDeploy.equals(200)) {
-    def dataDeploy = new JsonSlurperClassic().parseText(reqDeploy.getInputStream().getText())
-  }
-    
-  def repoName = "hello-cje" //scm.getUserRemoteConfigs()[0].getUrl().tokenize('/').last().split("\\.")
-  
-  def reqVerify = new URL("http://samson.zd-mini.com/projects/${repoName}/deploys/${dataDeploy.deploy_ids[0]}.json").openConnection();
-  reqVerify.setRequestMethod("GET")
-  reqVerify.setRequestProperty("Content-Type", "application/json")
-  reqVerify.setRequestProperty("Authorization", "Bearer ${conf.token}")
-    
-  def respVerify = reqVerify.getResponseCode();
-  if(respVerify.equals(200)) {
-    def dataVerify = new JsonSlurperClassic().parseText(reqVerify.getInputStream().getText())
-    println "${dataVerify}"
-  }
+    def samson = new Deploy(this, conf.host, conf.token, conf.webhook, conf.msg);
+    def deploy = samson.createDeploy();
+    def status = samson.getDeploy("${deploy.deploy_ids[0]}");
+    println "${status}"
 }
